6438119e5b3c20e32a4e2c698fd4dec9793abe6d
Manuel Ugalde <jmugaldeb@gmail.com>
2023-05-26 11:50:20 -0600

    Updated Auth Controller

--- Backend/UalaReplicaProject/Controllers/AuthenticateController.cs
+++ Backend/UalaReplicaProject/Controllers/AuthenticateController.cs
@@ -1,84 +1,85 @@
-﻿using CustomApi.Models.Identity;
-using DataAccess.Models.Identity;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
+﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using ComplyCube.Net;
-using ComplyCube.Net.Model;
 using ComplyCube.Net.Resources.Clients;
 using ComplyCube.Net.Resources.SDKTokens;
+using CustomApi.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace UalaReplicaProject.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class AuthenticateController : ControllerBase
 {
-    private readonly UserManager<ApplicationUser> userManager;
-    private readonly RoleManager<IdentityRole> roleManager;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
     private readonly IConfiguration _configuration;
 
-    public AuthenticateController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+    public AuthenticateController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
     {
-        this.userManager = userManager;
-        this.roleManager = roleManager;
+        _userManager = userManager;
+        _roleManager = roleManager;
         _configuration = configuration;
     }
 
     [HttpPost]
     [Route("Login")]
     public async Task<IActionResult> Login([FromBody] LoginModel model)
-    {       
-        var user = await userManager.FindByEmailAsync(model.Email);
-        if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
-        {
-            var userRoles = await userManager.GetRolesAsync(user);
-
-            var authClaims = new List<Claim>
-                {                    
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-                };
-            authClaims.Add(new Claim(ClaimTypes.Name, user.UserName));
-            authClaims.Add(new Claim(ClaimTypes.Email, user.Email));
-            foreach (var userRole in userRoles)
+    {
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            return BadRequest(new LoginResult
             {
-                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
-            }
-
-            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
-            var token = new JwtSecurityToken(
-                claims: authClaims,
-                expires: DateTime.UtcNow.AddDays(7),
-                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
-                );
-
-            return Ok(new LoginResult
-            {
-                Successful = true,
-                Token = new JwtSecurityTokenHandler().WriteToken(token),
-                Expiration = token.ValidTo,
-                Error = ""
+                Successful = false,
+                Error = "Username and password are invalid.",
+                Expiration = DateTime.Now.AddMinutes(-4),
+                Token = null
             });
-        }
-        return BadRequest(new LoginResult { Successful = false, Error = "Username and password are invalid.",Expiration = DateTime.Now.AddMinutes(-4),Token= null });
+        var roles = await _userManager.GetRolesAsync(user);
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Email,user.Email!),
+            new Claim(JwtRegisteredClaimNames.Name, user.UserName!)
+        };
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]!));
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha512Signature)
+        };
+        var jwt = new JwtSecurityTokenHandler().CreateToken(tokenDescriptor);
+        return Ok(new LoginResult
+        {
+            Successful = true,
+            Token = new JwtSecurityTokenHandler().WriteToken(jwt),
+            Expiration = jwt.ValidTo,
+            Error = ""
+        });
     }
 
     [HttpPost]
     [Route("Register")]
     public async Task<IActionResult> Register([FromBody] RegisterModel model)
     {
-        var userExists = await userManager.FindByNameAsync(model.Username);
+        var userExists = await _userManager.FindByNameAsync(model.Username);
         if (userExists != null)
             return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User already exists!" });
 
-        ApplicationUser user = new ApplicationUser()
+        IdentityUser user = new IdentityUser()
         {
             Email = model.Email,
             SecurityStamp = Guid.NewGuid().ToString(),
             UserName = model.Username
         };
-        var result = await userManager.CreateAsync(user, model.Password);
+        var result = await _userManager.CreateAsync(user, model.Password);
         if (!result.Succeeded)
             return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User creation failed! Please check user details and try again." });
 
@@ -87,21 +88,21 @@ public class AuthenticateController : ControllerBase
 
     [HttpPost]
     [Route("AddRole")]
-    public async Task<IActionResult> AddRole(string Role, string name)
+    public async Task<IActionResult> AddRole(string role, string name)
     {
-        var user = await userManager.FindByNameAsync(name);
+        var user = await _userManager.FindByNameAsync(name);
 
-        if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
-            await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
-        if (!await roleManager.RoleExistsAsync(UserRoles.User))
-            await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+        if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+        if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
 
-        if (await roleManager.RoleExistsAsync(UserRoles.Admin))
+        if (await _roleManager.RoleExistsAsync(role))
         {
-            await userManager.AddToRoleAsync(user, UserRoles.Admin);
+            await _userManager.AddToRoleAsync(user!, role);
         }
 
-        return Ok(new { Status = "Success", Message = "User created successfully!" });
+        return Ok(new { Status = "Success", Message = $"User \"{user}\" added to Role \"{role}\" successfully!" });
     }
 
     [HttpGet("GenerateToken/{id}")]
@@ -121,5 +122,3 @@ public class AuthenticateController : ControllerBase
         return Ok(sdkToken.token);
     }
 }
-
-
